b2cc374eeaed430ebcb01b2f2663193863d4c2a4
maddiemcm <mabmcmul@iu.edu>
2019-11-20 20:45:12 -0500

    Yay

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -6,11 +6,14 @@ var jump_speed = 6
 var spin = 0.1
 
 var velocity = Vector3()
+var jump = false
 
 func _physics_process(delta):
 	velocity += gravity * delta
 	get_input()
 	velocity = move_and_slide(velocity, Vector3.UP)
+	if jump and is_on_floor():
+		velocity.y = jump_speed
 	
 func get_input():
 	var vy = velocity.y
@@ -24,6 +27,9 @@ func get_input():
 	if Input.is_action_pressed("strafe_left"):
 		velocity += -transform.basis.x * speed
 	velocity.y = vy
+	jump = false
+	if Input.is_action_just_pressed("jump"):
+		jump = true
 	
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
